8699db8757b96dc3046c2e2bc6dff62e07039b10
Micheas Herman <m@micheas.net>
2018-12-06 19:37:37 -0800

    Add mysql port to docs
    Adding the mysql port to the docs.

--- container_engine/django_tutorial/mysite/settings.py
+++ container_engine/django_tutorial/mysite/settings.py
@@ -80,7 +80,9 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': {
         # If you are using Cloud SQL for MySQL rather than PostgreSQL, set
-        # 'ENGINE': 'django.db.backends.mysql' instead of the following.
+        # 'ENGINE': 'django.db.backends.mysql'
+        # 'PORT': '3306' 
+        # instead of the following.
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'polls',
         'USER': os.getenv('DATABASE_USER'),
